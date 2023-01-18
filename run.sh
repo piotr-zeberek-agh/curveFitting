7d54881a3baa5ce80bb5acede6c747e3fbabdb01
piotr-zeberek-agh <zeberek@student.agh.edu.pl>
2023-01-19 00:59:04 +0100

    updated run.sh

--- run.sh
+++ run.sh
@@ -1,2 +1,22 @@
+#!/bin/bash
 make "SMS=75"
-./curveFitting
+if [ -f curveFitting ]
+then
+	case $# in
+		2)
+			file=$1
+			nSamples=$(cat $1 | wc -l)
+			order=$2
+			
+			./curveFitting $file $nSamples $order
+			;;
+			
+		0)
+			./curveFitting
+			;;
+		*)
+			echo "Wrong argument count"
+	esac
+else
+	echo "Compile the program with make and specified number of SMS"
+fi
